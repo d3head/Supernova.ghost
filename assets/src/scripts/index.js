f2719e69fbeb177a9306ee8d86ab2f533c9a6fd6
d3head <hello@oleg.so>
2014-01-18 20:01:31 +0000

    Merge with 2.0

--- assets/src/scripts/index.js
+++ assets/src/scripts/index.js
@@ -12,3 +12,19 @@ var disqus_shortname = 'olegso'; // required: replace example with your forum sh
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
   }
 })();
+
+// Post cover. God, forgive me for jQuery, but Ghost don't have blog post cover feature
+var postCover = $('img[alt="image-cover"]'),
+    $blogCover = $('#blog-cover');
+
+if(postCover.length > 0) {
+  // Add image and class .covered
+  $blogCover.css('background-image', 'url(' + postCover.attr('src') + ')');
+  $blogCover.addClass('covered');
+
+  // Replace old content
+  $('#blog-image').hide();
+  $('#blog-title').text($('#post-title').hide().text());
+  $('#blog-subtitle').text($('#post-date').hide().text());
+  postCover.remove();
+}
